679aff149174484d2fccb45a5a5abf4f1b650bdc
Zarah Abubakar Kari <zarahabubakarkari@Mac.lan>
2025-12-21 13:17:32 +0100

    product controller

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+
 
 use Illuminate\Support\Facades\Route;
 
 
 
 Route::get("/",[HomeController::class,"index"])-> name("home");
-
+Route::apiRresource("/products", ProductController::class)->only(["index","show"]);
     
