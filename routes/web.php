f374725e7c708f2c450ee9843ea1934d8389eaa6
Zarah08 <zarahabubakar1234@gmail.com>
2025-12-22 23:31:33 +0100

    nothing done

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ use Illuminate\Support\Facades\Route;
 Route::get("/",[HomeController::class,"index"])-> name("home");
 Route::apiRresource("/products", ProductController::class)->only(["index","show"]); 
     
+//i did nothing today
